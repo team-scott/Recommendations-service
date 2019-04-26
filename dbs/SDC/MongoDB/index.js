ef06012012562424837fcc29964a6a78b3ee14f0
Vlad Igolnikov <vladigolnikov@gmail.com>
2019-04-26 11:56:40 -0400

    code cleanup

--- dbs/SDC/MongoDB/index.js
+++ dbs/SDC/MongoDB/index.js
@@ -2,10 +2,11 @@ const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017';
 
 
+
 const getRecs = (id, callback) => {
   MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
     if (err) throw err;
-    db = client.db('airbnb');
+    let db = client.db('airbnb');
     db.collection('recommendations').find({RoomId: +id}).toArray((err, results) => {
       if (err) throw err;
       callback(null, JSON.stringify(results));
--- server/server.js
+++ server/server.js
@@ -1,18 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
-const app = express();
-const port = process.env.PORT || 3001;
 const getRecs = require('../dbs/SDC/MongoDB/index.js')
 const cors = require('cors')
 
+const port = 3001;
+const app = express();
 app.use(cors())
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, '../public')));
-
-app.get('/:room', function(req, res) {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+app.use('/:room', express.static(path.join(__dirname, '../public')));
 
 app.get('/room/:room', (req, res) => {
   getRecs(req.params.room, (err, results) => {
@@ -24,5 +20,5 @@ app.get('/room/:room', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`server running at: http://localhost:${port}`);
+  console.log(`server running on port ${port}`);
 });
